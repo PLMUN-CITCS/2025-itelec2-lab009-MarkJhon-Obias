6e3ff707d41fa21ea1a6d1cf7419ac8fadedc33a
obiasmarkjhon_bsit@plmun.edu.ph <obiasmarkjhon_bsit@plmun.edu.ph>
2025-02-16 23:23:44 +0800

    Submitting Python Week 04 - Laboratory # 09

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,21 +1,18 @@
 def main():
-    """Main function to check the if the number is even or odd."""
+    """Main function to check if the number is even or odd."""
     
     try:
-        
-       user_input = input("Enter a number: ")
-    
-       number = int(user_input)
-    
+        user_input = input("Enter a number: ")
+        number = int(user_input)
 
-        #code here
-       if number % 2 == 0:
-           print("The number", number, "is Even.")
-       else:
-           print("The number", number, "is Odd.")
+        # Check if the number is even or odd
+        if number % 2 == 0:
+            print("The number", number, "is Even.")
+        else:
+            print("The number", number, "is Odd.")
              
     except ValueError:
-           print("Invalid input. Please enter an integer.")
+        print("Invalid input. Please enter an integer.")
     
 if __name__ == "__main__":
     main()
